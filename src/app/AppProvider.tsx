dbc3335794c9322f5ecfbafdd6c47135dfdb9a90
MinhLee2k <minh.nguyenle1809@gmail.com>
2022-11-01 21:14:47 +0700

    feat: Setup Provider with redux

---
+++ src/app/AppProvider.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from "react";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+
+interface AppProviderProps {}
+
+const AppProvider = ({ children }: PropsWithChildren<AppProviderProps>) => {
+  return <Provider store={store}>{children}</Provider>;
+};
+
+export default AppProvider;
